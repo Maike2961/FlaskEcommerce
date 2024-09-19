3213b81012a0763775e8d3e05ff8aa2e5614591a
mayki <mayki.silva@hotmail.com>
2024-09-19 15:07:25 -0300

    fix: erro na extends arquivo html

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 db = SQLAlchemy()
 login_manage = LoginManager()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mercado.db'
+""" app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mercado.db' """
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
 app.config["SECRET_KEY"]= '5b61281ebbac0ab023297a36'
 
 db.init_app(app)
